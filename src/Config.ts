3a20ac8e2b8cbbc3e17a7ba219305bb34f90fd5c
shreyansh <shreyanshm90051@gmail.com>
2026-05-30 19:54:52 +0530

    update backend url for prod

--- src/Config.ts
+++ src/Config.ts
@@ -1,5 +1,5 @@
 const Config = {
-  API_URL: 'https://886e-2401-4900-884b-67e4-fc85-d655-99d4-279b.ngrok-free.app',
+  API_URL: 'https://vns-track.de.r.appspot.com',
   GOOGLE_MAPS_API_KEY: 'AIzaSyCM3tM_LeJwxBrW-w9tLgV9tg7guOWT-BM',
   OTA_VERSION:       '0',
 };
